9a20ef0989646ff4b6b96ad90d1d08deecc9db1c
GeethasriIrudayasamy <geethasriirudayasamy@gmail.com>
2022-11-20 16:12:56 +0530

    Protecting The product Page

--- src/App.js
+++ src/App.js
@@ -1,36 +1,62 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
+import React, { useContext } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Home from "./Components/Pages/HomePage/Home";
 import Store from "./Components/Pages/StorePage/Store";
 import Contact from "./Components/Pages/ContactPage/Contact";
 import About from "./Components/Pages/AboutPage/About";
 import Login from "./Components/Pages/LoginPage/LoginPage";
 import CartProvider from "./Store/CartProvider";
+import AuthContext from "./Store/AuthContext";
 import ProductDetail from "./Components/Pages/ProductDetail/ProductDetail";
 
 function App() {
+    const authCtx = useContext(AuthContext);
+    const isLoggedIn = authCtx.isLoggedIn;
+
     return (
         <CartProvider>
-            <Switch>
-                <Route exact path="/about">
-                    <About />
-                </Route>
-                <Route exact path="/store">
-                    <Store />
-                </Route>
-                <Route exact path="/home">
-                    <Home />
-                </Route>
-                <Route path="/contact">
-                    <Contact />
-                </Route>
-                <Route exact path="/">
-                    <Login />
-                </Route>
-                <Route path="/store/:productId">
-                    <ProductDetail />
-                </Route>
-            </Switch>
+            {!isLoggedIn && (
+                <Switch>
+                    <Route path="/store" exact>
+                        <Redirect to="/" />
+                    </Route>
+                    <Route exact path="/about">
+                        <Redirect to="/" />
+                    </Route>
+                    <Route exact path="/home">
+                        <Redirect to="/" />
+                    </Route>
+                    <Route path="/contact">
+                        <Redirect to="/" />
+                    </Route>
+                    <Route path="/store/:productId">
+                        <Redirect to="/" />
+                    </Route>
+                    <Route exact path="/">
+                        <Login />
+                    </Route>
+                </Switch>
+            )}
+
+            {isLoggedIn && (
+                <Switch>
+                    <Route exact path="/about">
+                        <About />
+                    </Route>
+                    <Route exact path="/store">
+                        <Store />
+                    </Route>
+                    <Route exact path="/home">
+                        <Home />
+                    </Route>
+                    <Route path="/contact">
+                        <Contact />
+                    </Route>
+                    <Route path="/store/:productId">
+                        <ProductDetail />
+                    </Route>
+                </Switch>
+            )}
         </CartProvider>
     );
 }
